bb95e6fc9c10228ab883c367869c0d45f2299c64
EmyRub <esme.rubi@hotmail.com>
2025-06-20 16:43:59 -0600

    vista clientes

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,14 +1,13 @@
 "use client"
 
-import OrderCard from '@/components/order/OrderCard'
-import Heading from '@/components/ui/Heading'
-import { OrderWithProducts } from '@/src/types'
-import useSWR from 'swr'
+import useSWR from "swr";
+import Logo from "@/components/ui/Logo";
+import { OrderWithProducts } from "@/src/types";
+import LatestOrderItem from "@/components/order/LatestOrderItem";
 
+export default function OrdersPage() {
 
-export default function OrderPage() {
-
-    const url = '/admin/orders/api'
+    const url = '/orders/api'
 
     //Refresh automático
     const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
@@ -21,19 +20,20 @@ export default function OrderPage() {
 
     if (data) return (
         <>
-            <Heading>Administrar Ordenes</Heading>
+            <h1 className="text-center mt-20 text-6xl font-black">Ordenes Listas</h1>
 
+            <Logo />
 
             {data.length ? (
-                <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+                <div className="grid grid-cols-2 gap-5 max-w-5xl mx-auto mt-10">
                     {data.map(order => (
-                        <OrderCard
+                        <LatestOrderItem
                             key={order.id}
                             order={order}
                         />
                     ))}
                 </div>
-            ) : <p className='text-center'>No hay ordenes pendientes</p>}
+            ) : <p className="text-center my-10">No hay ordenes listas</p>}
 
         </>
     )
