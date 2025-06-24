9e415300c44bd696d8fd080f1999031786d231c5
EmyRub <esme.rubi@hotmail.com>
2025-06-23 19:09:47 -0600

    actualizacion final

--- app/admin/orders/api/route.ts
+++ app/admin/orders/api/route.ts
@@ -1,5 +1,8 @@
 import { prisma } from "@/src/lib/prisma"
 
+//Permite que vuelva a hacer la petición en vez de cacheo
+export const dynamic = 'force-dynamic'
+
 //Consulta la api, obtiene productos
 export async function GET() {
     const orders = await prisma.order.findMany({
